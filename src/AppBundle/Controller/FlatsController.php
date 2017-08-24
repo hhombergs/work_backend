518ce9b6a312fb80ffbe8deefdc8bd154f2c9d8e
Heinz Hombergs <hh@etacs.de>
2017-08-24 10:44:41 +0200

    Add the delete action

--- src/AppBundle/Controller/FlatsController.php
+++ src/AppBundle/Controller/FlatsController.php
@@ -27,7 +27,7 @@ class FlatsController extends FOSRestController
      * The doctrine bundle name
      */
     const FLATS_BUNDLE = 'AppBundle:Flats';
-    
+
     /**
      * @Rest\Get("/flats")
      */
@@ -39,15 +39,34 @@ class FlatsController extends FOSRestController
         }
         return $result;
     }
-    
+
     /**
      * @Rest\Get("/flats/{id}")
      */
-    public function idAction($id) {
+    public function idAction($id)
+    {
         $result = $this->getDoctrine()->getRepository(self::FLATS_BUNDLE)->find($id);
         if ($result === null || count($result) == 0) {
             return new View('Flat not found', Response::HTTP_NOT_FOUND);
         }
         return $result;
+    }
+
+    /**
+     * @Rest\Delete("/flats/{id}/{token}")
+     */
+    public function deleteAction($id, $token)
+    {
+        $sn = $this->getDoctrine()->getManager();
+        $flat = $this->getDoctrine()->getRepository(self::FLATS_BUNDLE)->find($id);
+        if ($flat === null || count($flat) == 0) {
+            return new View('Flat not found', Response::HTTP_NOT_FOUND);
+        }
+        if ($flat->getToken() !== $token) {
+            return new View('Access forbidden', Response::HTTP_FORBIDDEN);
+        }
+        $sn->remove($flat);
+        $sn->flush();
+        return new View("Deleted successfully", Response::HTTP_OK);
     }
 }
--- tests/AppBundle/Controller/FlatsControllerTest.php
+++ tests/AppBundle/Controller/FlatsControllerTest.php
@@ -7,18 +7,15 @@ class FlatsControllerTest extends WebTestCase
 {
     public function testGet() {
         $client = static::createClient();
-
         $client->request('GET', '/flats');
         $response = $client->getResponse();
         $content = json_decode($response->getContent(), TRUE);
-        
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertCount(2, $content);
     }
     
     public function testId() {
         $client = static::createClient();
-
         $client->request('GET', '/flats/1');
         $response = $client->getResponse();
         $content = json_decode($response->getContent(), TRUE);
@@ -26,5 +23,19 @@ class FlatsControllerTest extends WebTestCase
         $this->assertCount(8, $content);
         $this->assertArrayHasKey('id', $content);
         $this->assertEquals('2017-01-02T00:00:00+01:00', $content['enter_date']);
+    }
+    
+    public function testDelete() {
+        $client = static::createClient();
+        $client->request('DELETE', '/flats/3/I9xGz6iU');
+        $response = $client->getResponse();
+        $this->assertEquals(404, $response->getStatusCode());
+        $client->request('DELETE', '/flats/2/I9xGz6iUXXX');
+        $response = $client->getResponse();
+        $this->assertEquals(403, $response->getStatusCode());
+        $client->request('DELETE', '/flats/2/I9xGz6iU');
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('"Deleted successfully"', $response->getContent());
     }
 }
