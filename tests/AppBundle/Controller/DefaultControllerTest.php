6fe3e9e8f779d72dcb950940a9b0a92851cc0024
Heinz Hombergs <hh@etacs.de>
2017-08-24 09:15:10 +0200

    Change the DefaultController test to REST

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -10,7 +10,7 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
         $response = $client->getResponse();
         $this->assertEquals(403, $response->getStatusCode());
         $this->assertContains('Access forbidden', $response->getContent());
